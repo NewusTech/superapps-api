3aab891c72ce84c2c7eb91aa4eb62120c866a5c8
raiszidane95 <raiszidane95@gmail.com>
2024-08-01 15:51:17 +0700

    Fix namespace

--- app/Http/Controllers/API/Banner/BannerController.php
+++ app/Http/Controllers/API/Banner/BannerController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\Banner;
 
+use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use Exception;
 use Illuminate\Http\Request;
