115c7d4e8ff283a5b4800744c0e80487c7a844a5
raiszidane95 <raiszidane95@gmail.com>
2024-08-09 14:30:15 +0700

    Add order service, API riwayat pemesanan, details riwayat pemesanan

--- app/Http/Controllers/API/Pembayaran/PembayaranController.php
+++ app/Http/Controllers/API/Pembayaran/PembayaranController.php
@@ -227,13 +227,46 @@ class PembayaranController extends Controller
     public function getMetodePembayaran()
     {
         try {
+
             $data = MetodePembayaran::get();
-            $data->nama = $data->map(function ($item) {
-                return $item->metode;
-            });
+
+            if ($data->isEmpty()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Metode pembayaran tidak ditemukan'
+                ], 404);
+            }
+
+            $response = [
+                'payment_gateway' => [],
+                'bank_transfer' => [],
+                'cash' => null
+            ];
+
+            foreach ($data as $item) {
+                $metodeData = [
+                    'id' => $item->id,
+                    'nama' => $item->metode,
+                    'keterangan' => $item->keterangan,
+                    'kode' => $item->kode,
+                    'img' => $item->img
+                ];
+
+                switch ($item->kode) {
+                    case 1:
+                        $response['payment_gateway'][] = $metodeData;
+                        break;
+                    case 2:
+                        $response['bank_transfer'][] = $metodeData;
+                        break;
+                    case 3:
+                        $response['cash'] = $metodeData;
+                        break;
+                }
+            }
             return response()->json([
                 'success' => true,
-                'data' => $data,
+                'data' => $response,
                 'message' => 'Berhasil get data'
             ]);
         } catch (Exception $e) {
