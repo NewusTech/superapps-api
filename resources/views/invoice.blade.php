2b018a85291b1091a864878ed68a2b80cf10406c
raiszidane95 <raiszidane95@gmail.com>
2024-08-30 22:55:29 +0700

    Redesign Invoice

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -23,6 +23,19 @@
             margin: 0px;
         }
 
+        .table-data {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+
+        .table-data td {
+            border: 1px solid black;
+        }
+
+        .table-data thead tr {
+            color: white;
+        }
+
         body {
             margin: 0px;
         }
@@ -43,112 +56,133 @@
     </style>
 </head>
 
-<body style="padding: 40px; font-family:'Nunito', sans-serif;">
-    <table style="width: 100%;">
-        <tr>
-            <td>
-                <!-- HEADER -->
-                <table id="header" style="table-layout: auto;">
-                    <tr>
-                        <td>
-                            <table style="table-layout: auto; width: 100%;">
-                                <tr>
-                                    <td style="width: 75px;">
-                                        <img src="{{ public_path('assets/Icon.png') }}" alt="Logo" style="width: 90px; height: 90px">
-                                    </td>
-                                    <td>
-                                        <h1 style="font-weight: bold;">RAMA TRANZ</h1>
-                                        <p>PT. RASYA MANDIRI TRANZ</p>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-                <!-- BODY -->
-                <table style="table-layout: auto; width: 75%;">
-                    <tr>
-                        <td style="padding:0;">
-                            <div>
-                                <p><strong>Invoice</strong></p>
-                                <p>{{$data->pesanan->invoice}}</p>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="padding:0;">
-                            <div>
-                                <p><strong>Nama Pemesan</strong></p>
-                                <p>{{$data->pesanan->nama}}</p>
-                            </div>
-                            <div style="margin-top: 10px;">
-                                <p><strong>Metode Pembayaran</strong></p>
-                                <p>{{$data->pesanan->metode_pembayaran}}</p>
-                            </div>
-                        </td>
-                        <td style="padding:0;">
-                            <div style="margin-top: 10px;">
-                                <p><strong>No. Telepon</strong></p>
-                                <p>{{$data->pesanan->no_telp}}</p>
-                            </div>
-                            <div>
-                                <p><strong>Tanggal</strong></p>
-                                <p>{{$data->pembayaran->tanggal}}</p>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-                <div style="margin-top: 20px;"> <strong>Detail Penumpang</strong></div>
-                <table style="table-layout: auto; width: 100%;">
-                    <thead>
-                        <tr style="background-color: #E8E8E8;">
-                            <td><strong>Nama</strong></td>
-                            <td><strong>NIK</strong></td>
-                            <td><strong>Email</strong></td>
-                            <td><strong>No. Tlp</strong></td>
-                            <td><strong>No. Kursi</strong></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($data->penumpang as $penumpang)
+<body style="font-family:'Nunito', sans-serif;">
+    <div style="width: 100%; height: 69px; background-color: #3572EF">
+        <div style="text-align: center; padding-top: 25px; font-size: 17px; color:white">E-tiket Keberangkatan Travel</div>
+    </div>
+    <div style="padding:0px 40px;">
+        <table style="width: 100%;">
+            <tr>
+                <td>
+                    <!-- HEADER -->
+                    <table id="header" style="table-layout: auto;">
                         <tr>
-                            <td>{{$penumpang->nama}}</td>
-                            <td>{{$penumpang->nik}}</td>
-                            <td>{{$penumpang->email}}</td>
-                            <td>{{$penumpang->no_telp}}</td>
-                            <td>{{$penumpang->kursi}}</td>
+                            <td>
+                                <table style="table-layout: auto; width: 100%;">
+                                    <tr>
+                                        <td style="width: 75px;">
+                                            <img src="{{ public_path('assets/Icon.png') }}" alt="Logo" style="width: 90px; height: 90px">
+                                        </td>
+                                        <td>
+                                            <h1 style="font-weight: bold;">RAMA TRANZ</h1>
+                                            <p>PT. RASYA MANDIRI TRANZ</p>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
                         </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                    </table>
+                    <hr>
+                    <div style="font-size: 20px; font-weight: bold; padding:20px 0px">Invoice Tiket Travel</div>
+                    <!-- BODY -->
+                    <table style="table-layout: auto; width: 100%;">
+                        <tr>
+                            <td style="padding-top:15px;">
+                                <div>
+                                    <p><strong>Nama Pemesan</strong></p>
+                                    <p>{{$data->pesanan->nama}}</p>
+                                </div>
+                            </td>
+                            <td style="padding-top:15px;">
+                                <div>
+                                    <p><strong>No. Telepon</strong></p>
+                                    <p>{{$data->pesanan->no_telp}}</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top:15px;">
+                                <div>
+                                    <p><strong>No. Invoice</strong></p>
+                                    <p>{{$data->pesanan->invoice}}</p>
+                                </div>
+                            </td>
+                            <td style=" padding-top:15px;">
+                                <div>
+                                    <p><strong>Metode Pembayaran</strong></p>
+                                    <p>{{$data->pembayaran->metode}}</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding-top:15px;">
+                                <div>
+                                    <p><strong>Waktu Pembayaran</strong></p>
+                                    <p>{{$data->pembayaran->jam}}</p>
+                                </div>
+                            </td>
+                            <td style=" padding-top:15px;">
+                                <div>
+                                    <p><strong>Tanggal Pembayaran</strong></p>
+                                    <p>{{$data->pembayaran->tanggal}}</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <hr>
+        <div style="font-size: 20px; font-weight: bold;  padding:20px 0px">Detail Penumpang</div>
+        <table class="table-data" style="table-layout: auto; width: 100%; margin-bottom: 20px; ">
+            <thead>
+                <tr style="background-color: #3572EF;">
+                    <td><strong>Nama</strong></td>
+                    <td><strong>NIK</strong></td>
+                    <td><strong>Email</strong></td>
+                    <td><strong>No. Tlp</strong></td>
+                    <td><strong>No. Kursi</strong></td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data->penumpang as $penumpang)
+                <tr>
+                    <td>{{$penumpang->nama}}</td>
+                    <td>{{$penumpang->nik}}</td>
+                    <td>{{$penumpang->email}}</td>
+                    <td>{{$penumpang->no_telp}}</td>
+                    <td>{{$penumpang->kursi}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-                <!-- DETAIL PENUMPANG -->
-                <div style="margin-top: 20px;"> <strong>Detail Pembayaran</strong></div>
-                <table style="table-layout: auto; width: 100%;">
-                    <thead>
-                        <tr style="background-color: #E8E8E8">
-                            <td style="text-align: center">
-                                <strong>Jumlah Tiket</strong>
-                            </td>
-                            <td style="text-align: center">
-                                <strong>Harga Tiket</strong>
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td style="text-align: center">{{$data->pembayaran->jumlah_tiket}}</td>
-                            <td style="text-align: center">Rp.{{number_format($data->pembayaran->harga_tiket,0,',','.')}}</td>
-                        </tr>
-                        <tr>
-                            <td style="text-align: center"><strong>Total Harga</strong></td>
-                            <td style="text-align: center">Rp.{{number_format($data->pembayaran->total_harga,0,',','.')}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </td>
-        </tr>
-    </table>
+        <!-- DETAIL PENUMPANG -->
+        <hr>
+        <div style="font-size: 20px; font-weight: bold; padding:20px 0px">Detail Pembayaran</div>
+        <table class="table-data" style="table-layout: auto; width: 100%;">
+            <thead>
+                <tr style="background-color: #3572EF">
+                    <td style="text-align: center">
+                        <strong>Jumlah Tiket</strong>
+                    </td>
+                    <td style="text-align: center">
+                        <strong>Harga Tiket</strong>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="text-align: center">{{$data->pembayaran->jumlah_tiket}}</td>
+                    <td style="text-align: center">Rp.{{number_format($data->pembayaran->harga_tiket,0,',','.')}}</td>
+                </tr>
+                <tr>
+                    <td style="text-align: center"><strong>Total Harga</strong></td>
+                    <td style="text-align: center">Rp.{{number_format($data->pembayaran->total_harga,0,',','.')}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
