e25dd380b40ad6fa888397a7405797f83062163d
raiszidane95 <raiszidane95@gmail.com>
2024-09-03 11:45:02 +0700

    add update photo profile

--- app/Http/Controllers/API/Users/UserController.php
+++ app/Http/Controllers/API/Users/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\Users;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,25 +31,65 @@ class UserController extends Controller
 
     public function updateProfile(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'nama' => 'required',
-            'nik' => 'required',
-            'alamat' => 'required',
-            'no_telp' => 'required|min:8|max:13',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['message' => $validator->errors()->first()], 422);
+        try {
+            $validator = Validator::make($request->all(), [
+                'nama' => 'required',
+                'nik' => 'required',
+                'alamat' => 'required',
+                'no_telp' => 'required|min:8|max:13',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['message' => $validator->errors()->first()], 422);
+            }
+            $authUser = auth()->user();
+            $user = User::findOrFail($authUser->id);
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+            $user->update($request->all());
+            return response()->json([
+                'success' => true,
+                'data' => $user,
+                'message' => 'Berhasil update user'
+            ]);
+        } catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
         }
-        $authUser = auth()->user();
-        $user = User::findOrFail($authUser->id);
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-        $user->update($request->all());
-        return response()->json([
-            'success' => true,
-            'data' => $user,
-            'message' => 'Berhasil update user'
-        ]);
     }
+
+    public function profilePhotoUpdate(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+
+            if ($validator->fails()) {
+                throw new Exception($validator->errors()->first());
+            }
+
+            $user = auth()->user();
+            $user = User::findOrFail($user->id);
+            $data = $request->all();
+
+            if ($request->hasFile('image_url')) {
+                $file = $request->file('image_url');
+                $gambarPath = $file->store('superapps/profile', 's3');
+                $fullUrl = 'https://' . env('AWS_BUCKET') . '.' . 's3' . '.' . env('AWS_DEFAULT_REGION') . '.' . 'amazonaws.com/' . $gambarPath;
+                $data['image_url'] = $fullUrl;
+            } else {
+                $data['image_url'] = null;
+            }
+
+            $user->update($data);
+            return response()->json([
+                'success' => true,
+                'data' => $user,
+                'message' => 'Berhasil update profile photo'
+            ]);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
 }
