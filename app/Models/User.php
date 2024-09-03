089ac007f09cf330da43eb7bea7a67ddc413bb7f
raiszidane95 <raiszidane95@gmail.com>
2024-09-03 11:45:40 +0700

    add kota

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable implements JWTSubject
         'email',
         'no_telp',
         'alamat',
+        'kota',
         'nik',
         'password',
         'role_id',
